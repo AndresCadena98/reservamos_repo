d77a37ce50196041c7fb9fd70b1d68a5287a2a8e
AndresCadenaAlanis <108959131+AndresCadenaAlanis@users.noreply.github.com>
2023-01-31 19:18:23 -0600

    Revert "first commit"

--- lib/domain/repositories/weat_repo.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:reservamos/domain/failures/failures.dart';
-import '../entities/weatlogic_entity.dart';
-
-abstract class WeatRepo {
-  Future<Either<Failure, WeatLogicEntity>> getWeatherLogicFromDatasource(String place);
-}
